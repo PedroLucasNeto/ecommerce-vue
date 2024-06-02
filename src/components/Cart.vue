fabcfcb7c8a9308736b44b8a0aee06e77d36dd93
Pedro Lucas <101678622+PedroLucasNeto@users.noreply.github.com>
2024-06-02 15:57:21 -0300

    fix: cart button

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="drawer drawer-end">
     <input id="my-drawer-4" type="checkbox" class="drawer-toggle" v-model="cartStore.isCartOpen" />
-    <div class="drawer-content">
-      <!-- Page content here -->
-      <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
-    </div>
+
     <div class="drawer-side ">
       <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content relative">
