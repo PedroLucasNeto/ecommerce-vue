818e47bee555e1ef2f4aa7cbc24ec6a381c31e73
brenooduarte <brenoo.duarte@hotmail.com>
2024-06-02 16:07:28 -0300

    fix: change product placehold

--- src/pages/management/components/products/ProductsForm.vue
+++ src/pages/management/components/products/ProductsForm.vue
@@ -1,55 +1,88 @@
 <template>
-  <form ref="formProduct" class="flex flex-col shadow-xl space-y-4 p-4 shadow rounded-lg w-fit bg-base-100 py-4">
+  <form
+    ref="formProduct"
+    class="flex flex-col shadow-xl space-y-4 p-4 shadow rounded-lg w-fit bg-base-100 py-4"
+  >
     <div v-if="product.id">
       <label>Id</label>
-      <input type="text" class="input input-bordered w-full max-w-xs" v-model="product.id" disabled />
+      <input
+        type="text"
+        class="input input-bordered w-full max-w-xs"
+        v-model="product.id"
+        disabled
+      />
     </div>
     <label>Nome</label>
-    <input type="text" placeholder="Ex: Tênis" class="input input-bordered w-full max-w-xs" v-model="product.name" />
+    <input
+      type="text"
+      placeholder="Ex: Tênis"
+      class="input input-bordered w-full max-w-xs"
+      v-model="product.name"
+    />
     <label>Preço</label>
-    <input type="number" placeholder="Ex: Tênis Adidas" class="input input-bordered w-full max-w-xs"
-      v-model="product.price" />
+    <input
+      type="number"
+      placeholder="Ex: 150"
+      class="input input-bordered w-full max-w-xs"
+      v-model="product.price"
+    />
     <label>Descrição</label>
-    <input type="text" placeholder="Ex: Tênis Adidas" class="input input-bordered w-full max-w-xs"
-      v-model="product.description" />
+    <input
+      type="text"
+      placeholder="Ex: Tênis Adidas"
+      class="input input-bordered w-full max-w-xs"
+      v-model="product.description"
+    />
     <label>Imagem</label>
-    <input type="file" accept="image/*" class="file-input file-input-bordered w-full max-w-xs"
-      @change="setProductImage" />
-    <button v-if="!product.id" class="btn btn-success w-fit text-white" @click.prevent="saveProduct">Salvar</button>
-    <button v-else class="btn btn-success w-fit text-white" @click.prevent="editProduct">Alterar</button>
+    <input
+      type="file"
+      accept="image/*"
+      class="file-input file-input-bordered w-full max-w-xs"
+      @change="setProductImage"
+    />
+    <button
+      v-if="!product.id"
+      class="btn btn-success w-fit text-white"
+      @click.prevent="saveProduct"
+    >
+      Salvar
+    </button>
+    <button v-else class="btn btn-success w-fit text-white" @click.prevent="editProduct">
+      Alterar
+    </button>
   </form>
 </template>
 
 <script setup>
-import productsService from '@/api/productsService.js';
+import productsService from '@/api/productsService.js'
 
 const product = defineModel('product', {
   type: Object
 })
 
 function clearForm() {
-  product.value.id = null;
-  product.value.name = '';
-  product.value.price = 0;
-  product.value.description = '';
-  product.value.image = '';
+  product.value.id = null
+  product.value.name = ''
+  product.value.price = 0
+  product.value.description = ''
+  product.value.image = ''
 }
 
 function setProductImage(e) {
-  const file = e.target.files[0];
+  const file = e.target.files[0]
   if (file) {
-    const reader = new FileReader();
+    const reader = new FileReader()
     reader.onload = (event) => {
-      product.value.image = event.target.result;
-    };
-    reader.readAsDataURL(file);
+      product.value.image = event.target.result
+    }
+    reader.readAsDataURL(file)
   }
 }
 
 async function saveProduct() {
   try {
-    await productsService.createProduct(product.value);
-    clearForm();
+    await productsService.createProduct(product.value)
+    clearForm()
   } catch (error) {
     console.log(error)
   }
@@ -57,8 +90,8 @@ async function saveProduct() {
 
 async function editProduct() {
   try {
-    await productsService.updateProductById(product.value.id, product.value);
-    clearForm();
+    await productsService.updateProductById(product.value.id, product.value)
+    clearForm()
   } catch (error) {
     console.log(error)
   }
